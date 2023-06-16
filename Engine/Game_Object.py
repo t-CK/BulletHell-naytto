efda433f318926e80ef9487011c1c6b4e272c890
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-06-16 08:30:59 +0300

    Game_Object -luokan luonti

---
+++ Engine/Game_Object.py
@@ -0,0 +1,12 @@
+from Components import Game_Component
+
+class Game_Object:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def update(self, delta_time :float):
+        pass
+    
+    def update_components(self):
+        pass
