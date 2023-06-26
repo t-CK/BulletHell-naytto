181035c43860d6b2c0d0dfc9e0cfd1775f5deea6
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-06-26 18:59:12 +0300

    GameLoopin siivous ja pause systeemin tekeminen (vaatii jalostamista)

--- Engine/Game.py
+++ Engine/Game.py
@@ -1,9 +1,9 @@
-from Log import *
-import Game_Object
-import Window
-import array
-import time
+import Log
+from pygame import event
+from pygame import key
+from pygame import time
 from enum import Enum
+
 
 
 # Enum luokka pelin statuksen seuraamista varten
@@ -16,34 +16,51 @@ class Game:
     # Ainoastaan yksi instanssi pelist� sallitaan kerrallaan, joten Game -luokka voi olla staattinen (Luokan muuttujat tallennetaan suoraan class-variableina)
     _is_Running :bool
     _state :Game_State
-    _game_objects :list
+    _game_objects = []
+#    _wnd :Window.Window
+    # delta time
+    _prev_tick = 0
     _delta_time = 0.0
-    _wnd :Window.Window
 
 
     def __init__(self) -> None:
-        self._wnd = Window.Window()
+#        self._wnd = Window.Window()
         self._state = Game_State(2)
         self._is_Running = True
 
-    def add_game_object(self, object :Game_Object) -> None:
-        """Lisää uuden peliobjektin peliin"""
-        # Tarkastetaan funktioon sy�tetyn parametrin tyyppi ja tyypin ollessa Game_Object lis�t��n se listaan
-        # Muussa tapauksessa tulostetaan vir�heilmoitus
-        if type(object) == Game_Object:
-            self._game_objects.append(Game_Object())
-            return
-        Log_Error("Game.add_game_object() syötettiin vääränlainen parametri")
 
     def game_loop(self):
-        # Ajanoton k�ynnistys
         while self._is_Running:
+            # Jos game_state on PAUSE, asetetaan prev_tick arvoksi 0, tarkastetaan onko escape näppäintä painettu pausen lopettamiseksi
+            # ja hypätään loopin alkuun
+            if self._state == Game_State.PAUSED:
+                self._prev_tick = 0.0
+                if event.peek():
+                    keys = key.get_pressed()
+                    e = event.poll()
+                    if keys[event.K_ESCAPE]:
+                        self._state = 0
+                Log.Log_Info("PAUSED")
+                continue
+            if event.peek():
+                e = event.poll()
+                if keys[event.K_ESCAPE]:
+                    self._state = Game_State.PAUSED
+            Log.Log_Info(self._delta_time)
+            # Lasketaan delta time ja tallennetaan pygame.get_ticks() palauttama arvo prev_tick muuttujaan
+            if self._prev_tick == 0.0:
+                self._prev_tick = time.get_ticks()
+            else:
+                self._delta_time = (time.get_ticks() - self._prev_tick) / 1000
+            # Tyhjennetään ikkunan sisältö ja renderöidään taustaväri
+#            self._wnd.draw_background()
             # Loopataan peliobjektien lista ja päivitetään objektit + komponentit
             for instance in self._game_objects:
-                instance.update(self._delta_time)
-            # Ikkunan sisällön päivittäminen
+                print(self._delta_time)
+                # Päivitetään peliobjektit (peliobjekti päivittää komponenttinsa ja renderöi spritet)
+#                instance.update(delta_time=self._delta_time)
+            
 
-        # Ajanoton pys�ytys ja _delta_time -muuttujan p�ivitys 
 
     def get_delta_time(self) -> float:
         return self._delta_time
