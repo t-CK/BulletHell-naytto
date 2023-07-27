60147d12871eb7f6022792a28c93493a1648e2e4
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-07-27 11:23:34 +0300

    Gettereiden ja updaten lisäys Counter -luokkaan

--- Counter.py
+++ Counter.py
@@ -1,17 +1,28 @@
-
+import time
 
 class Counter:
     _kill_count :int
-    _timer :float
+    _timer_start :float
     
-    def __init__(self, kill = 0, time = 0.0):
+    def __init__(self, kill = 0, timer = 0.0):
         self._kill_count = kill
-        self._timer = time
+        if time > 0:
+            self._timer_start = timer
+        # Luodaan ajastin -objekti laskemaan kulunutta peliaikaa
+        else: self._timer_start = time.time()
         
     def kill_update(self):
         """Kasvattaa kill counteria yhdellä\n
         Kutsutaan aina tapon yhteydessä"""
         self._kill_count +=1
     
+    def timer_update(self):
+        pass
+    
     def get_kill_count(self):
         return self._kill_count
+    
+    def get_timer(self):
+        """Palauttaa pelin alusta kuluneen ajan sekunneissa"""
+        now = time.time()
+        return time.time() - self._timer_start
