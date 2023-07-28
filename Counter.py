bb02cb7253637e59eb06903511d35cc8a19dc336
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-07-28 11:54:17 +0300

    UI renderöinnin lisäys Counter -luokkaan. doc stringien lisäys Counter luokkaan. Counter luokkaan lisätty pygame -ikkuna(Surface) referenssi UI:n renderöintiä varten

--- Counter.py
+++ Counter.py
@@ -1,15 +1,22 @@
 import time
+from pygame import display, surface
 
 class Counter:
     _kill_count :int
     _timer_start :float
     
-    def __init__(self, kill = 0, timer = 0.0):
+    def __init__(self, wnd :surface.Surface, kill = 0, timer = 0.0):
+        """Counter luokan initialisointi.\n
+        Parametrit:
+        1. referenssi pygame -ikkunaan
+        2. tappojen määrä (ladattaessa peli tallennuksesta), oletusarvona 0
+        3. aikalaskuri (ladattaessa peli tallennuksesta), oletusarvona 0.0"""
         self._kill_count = kill
         if time > 0:
             self._timer_start = timer
         # Luodaan ajastin -objekti laskemaan kulunutta peliaikaa
         else: self._timer_start = time.time()
+        self._wnd = wnd
         
     def kill_update(self):
         """Kasvattaa kill counteria yhdellä\n
@@ -20,6 +27,10 @@ class Counter:
         return self._kill_count
     
     def get_timer(self):
-        """Palauttaa pelin alusta kuluneen ajan sekunneissa"""
+        """Palauttaa pelin alusta kuluneen ajan sekuntteina"""
         now = time.time()
         return time.time() - self._timer_start
+    
+    def render_counter_ui(self):
+        """Renderöi counter UI:t"""
+        pass
