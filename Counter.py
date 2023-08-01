28d613b725034026aa9f365d8bfd9e89ce330e00
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-08-01 13:16:58 +0300

    Window referenssin lisäys Counter luokan __init__ -metodiin

--- Counter.py
+++ Counter.py
@@ -1,12 +1,16 @@
 import time
+from Window import Window
+from pygame import surface, font
 
 class Counter:
     _kill_count :int
     _timer :float
     
-    def __init__(self, kill = 0, timer = 0.0):
+    def __init__(self, wnd :Window, kill = 0, timer = 0.0):
         self._kill_count = kill
         self._timer
+        self._font = font.SysFont(size=20)
+        self._wnd = wnd
         
     def kill_update(self):
         """Kasvattaa kill counteria yhdellä\n
@@ -24,3 +28,10 @@ class Counter:
     def get_timer(self):
         """Palauttaa pelin alusta kuluneen ajan sekunneissa"""
         return self._timer
+    
+    def render_counters(self) -> None:
+        """Render counters on screen"""
+
+        # Render counters as text
+        render_surf = font.Font.render(text=f"KILLS: {self._kill_count}\nTIME: {self._timer}", color=(255, 10, 25, 1), background=None)
+        self._wnd._wnd.blit(render_surf, (20, 50))
