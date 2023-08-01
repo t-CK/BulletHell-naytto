3a25202f3477910b7c50c13d8a1ab6f5a92bd6fd
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-08-01 09:59:17 +0300

    Aikalaskurin päivittäminen käyttämänän delta_timea

--- Counter.py
+++ Counter.py
@@ -2,27 +2,25 @@ import time
 
 class Counter:
     _kill_count :int
-    _timer_start :float
+    _timer :float
     
     def __init__(self, kill = 0, timer = 0.0):
         self._kill_count = kill
-        if time > 0:
-            self._timer_start = timer
-        # Luodaan ajastin -objekti laskemaan kulunutta peliaikaa
-        else: self._timer_start = time.time()
+        self._timer
         
     def kill_update(self):
         """Kasvattaa kill counteria yhdellä\n
         Kutsutaan aina tapon yhteydessä"""
         self._kill_count +=1
     
-    def timer_update(self):
-        pass
+    def timer_update(self, delta_time :float):
+        # Päivittää self._timer muuttujan
+        # Ottaa parametrina delta_timen (float)
+        self._timer += delta_time
     
     def get_kill_count(self):
         return self._kill_count
     
     def get_timer(self):
         """Palauttaa pelin alusta kuluneen ajan sekunneissa"""
-        now = time.time()
-        return time.time() - self._timer_start
+        return self._timer
