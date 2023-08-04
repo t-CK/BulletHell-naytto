5ed4bb8c0dbfc2957d3274d12df81cb2a3310284
denimJesus <113284148+denimJesus@users.noreply.github.com>
2023-08-04 17:06:03 +0300

    moved remaining functions inside renamed class
    Improved indentation, renamed Game() to App() to avoid conflict/confusion with class in Game.py, etc.

--- main.py
+++ main.py
@@ -6,92 +6,90 @@ from variables import *
 
 pg.init()
 
-class Game():
+class App():
     def __init__(self):
         self.ticks = 0
         self.player = Player()
         self.initialize_game()
+        self.spawn_timer = STARTING_SPAWN_TIME
 
     def initialize_game(self):
         """ Initialize player, Ui etc. """
         self.player.rect.center = (WIDTH//2, HEIGHT//2)
         ui.Ui_Bar_XP(self.player)
         ui.Ui_Bar_Health(self.player)
-        initialize_level()
+        self.initialize_level()
     
     def main(self):
         """ Main loop """
         while True:
             event_queue.process_event_queue(self)
-            spawn_enemies()
+            self.spawn_enemies()
             self.player.update()
             all_sprites.update()
-            check_collisions()
-            render_screen()
+            self.check_collisions()
+            self.render_screen()
             clock.tick(FPS)
             self.ticks += 1
 
-def initialize_level():
-    """ Initialize level. Just spawn a few random obstacles on screen for now. """
-    global spawn_timer
-    for _ in range(WIDTH // 150):
-        size = (size_x, size_y) = (random.randint(20*SPRITE_SCALE, 100*SPRITE_SCALE),
-                                   random.randint(20*SPRITE_SCALE, 100*SPRITE_SCALE))
-        position = (pos_x, pos_y) = (random.randint(0, WIDTH), random.randint(0, HEIGHT))
-        while abs(pos_x - WIDTH) < 50 + size_x or abs(pos_y - HEIGHT) < 50 + size_y:
+    def initialize_level(self):
+        """ Initialize level. Just spawn a few random obstacles on screen for now. """
+        for _ in range(WIDTH // 150):
+            size = (size_x, size_y) = (random.randint(20*SPRITE_SCALE, 100*SPRITE_SCALE),
+                                       random.randint(20*SPRITE_SCALE, 100*SPRITE_SCALE))
             position = (pos_x, pos_y) = (random.randint(0, WIDTH), random.randint(0, HEIGHT))
-        world.World(*position, *size)
-    spawn_timer = STARTING_SPAWN_TIME
+            while abs(pos_x - WIDTH) < 50 + size_x or abs(pos_y - HEIGHT) < 50 + size_y:
+                position = (pos_x, pos_y) = (random.randint(0, WIDTH), random.randint(0, HEIGHT))
+            world.World(*position, *size)
+    
 
-def spawn_enemies():
-    """ Spawns enemies at decreasing intervals, starting at STARTING_SPAWN_TIME ticks apart """
-    global spawn_timer
-    spawn_timer -= 1
-    if spawn_timer == 0:
-        # Randomize spawn direction (up, right, down, left) and set x and y to be a bit off-screen on that side
-        spawn_side = random.randrange(4)
-        if spawn_side == 0:   # Up
-            x = random.randint(-WIDTH * 0.2, WIDTH * 1.2)
-            y = -30
-        elif spawn_side == 1: # Right
-            x = WIDTH + 30
-            y = random.randint(-HEIGHT * 0.3, HEIGHT * 1.3)
-        elif spawn_side == 2: # Down
-            x = random.randint(-WIDTH * 0.2, WIDTH * 1.2)
-            y = HEIGHT + 30
-        elif spawn_side == 3: # Left
-            x = -30
-            y = random.randint(-HEIGHT * 0.3, HEIGHT * 1.3)
-        enemies.Enemy(game, (x,y))
-        spawn_timer = STARTING_SPAWN_TIME - game.ticks//100 if STARTING_SPAWN_TIME - game.ticks//100 > 10 else 10
+    def spawn_enemies(self):
+        """ Spawns enemies at decreasing intervals, starting at STARTING_SPAWN_TIME ticks apart """
+        self.spawn_timer -= 1
+        if self.spawn_timer == 0:
+            # Randomize spawn direction (up, right, down, left) and set x and y to be a bit off-screen on that side
+            spawn_side = random.randrange(4)
+            if spawn_side == 0:   # Up
+                x = random.randint(-WIDTH * 0.2, WIDTH * 1.2)
+                y = -30
+            elif spawn_side == 1: # Right
+                x = WIDTH + 30
+                y = random.randint(-HEIGHT * 0.3, HEIGHT * 1.3)
+            elif spawn_side == 2: # Down
+                x = random.randint(-WIDTH * 0.2, WIDTH * 1.2)
+                y = HEIGHT + 30
+            elif spawn_side == 3: # Left
+                x = -30
+                y = random.randint(-HEIGHT * 0.3, HEIGHT * 1.3)
+            enemies.Enemy(self, (x,y))
+            self.spawn_timer = max(10, STARTING_SPAWN_TIME - self.ticks//100)
 
-def check_collisions():
-    """ Checks for non-movement related collision.
-
-    Checks collision of bullets/enemies and enemies/player and deals damage for now.
-    Movement related collision is in each sprite's update() function, and checking
-    distance for pickups happens in the pickup's update().
-    """
-    for sprite in enemy_group:
-        if pg.sprite.spritecollideany(sprite, bullet_group):
-            sprite.damage()
-        if pg.sprite.collide_rect_ratio(1.01)(sprite, game.player):
-            game.player.damage(sprite.dmg)
-            sprite.damage()
-
-def render_screen():
-    """ Fill background, blit sprites and flip() the screen """
-    SCREEN.fill((20,20,150))
-    # for sprite in all_sprites:
-        # SCREEN.blit(sprite.surf, sprite.rect)
-    for group in (items_group, enemy_group, world_group, bullet_group):
-        for sprite in group:
+    def check_collisions(self):
+        """ Checks for non-movement related collision.
+    
+        Checks collision of bullets/enemies and enemies/player and deals damage for now.
+        Movement related collision is in each sprite's update() function, and checking
+        distance for pickups happens in the pickup's update().
+        """
+        for sprite in enemy_group:
+            if pg.sprite.spritecollideany(sprite, bullet_group):
+                sprite.damage()
+            if pg.sprite.collide_rect_ratio(1.01)(sprite, self.player):
+                self.player.damage(sprite.dmg)
+                sprite.damage()
+    
+    def render_screen(self):
+        """ Fill background, blit sprites and flip() the screen """
+        SCREEN.fill((20,20,150))
+        # for sprite in all_sprites:
+            # SCREEN.blit(sprite.surf, sprite.rect)
+        for group in (items_group, enemy_group, world_group, bullet_group):
+            for sprite in group:
+                SCREEN.blit(sprite.surf, sprite.rect)
+        SCREEN.blit(self.player.surf, self.player.rect)
+        for sprite in ui_group:
             SCREEN.blit(sprite.surf, sprite.rect)
-    SCREEN.blit(game.player.surf, game.player.rect)
-    for sprite in ui_group:
-        SCREEN.blit(sprite.surf, sprite.rect)
-    pg.display.flip()
-
+        pg.display.flip()
+    
 if __name__ == "__main__":
-    game = Game()
-    game.main()
+    App().main()
