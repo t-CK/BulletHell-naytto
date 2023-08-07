69d2a58fa7611a9292e4444d70e6e0f06cbe457e
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-08-07 09:33:45 +0300

    High score SQL -yhteys ja python -luokan viimeistely

--- Tests/profiles-high_score/testmain.py
+++ Tests/profiles-high_score/testmain.py
@@ -4,4 +4,6 @@ if __name__ == "__main__":
     hs = High_Score()
     hs.add_score("Testi", 10, 100, 5.0)
     hs.add_score("sdlkfj", 58, 150, 8.5)
-    print(hs.get_scores())
+    print("CASE 1:\n", hs.get_scores(), "\n")
+    hs.update_score("Testi", 50, 200, 20.5)
+    print("CASE 2\n", hs.get_scores())
